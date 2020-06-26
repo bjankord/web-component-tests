b7a7184b89d8c4d3afe3e512a7283ec80f18cd11
Brett <bjankord@gmail.com>
2020-06-26 14:29:43 -0500

    Remove WWW from version control

--- www/build/app.esm.js
+++
@@ -1,7 +0,0 @@
-import { b as bootstrapLazy } from './index-f56d61f5.js';
-import { p as patchBrowser, g as globalScripts } from './app-globals-89f16005.js';
-
-patchBrowser().then(options => {
-  globalScripts();
-  return bootstrapLazy([["button-component",[[4,"button-component",{"text":[1]}]]],["card-component",[[0,"card-component",{"name":[1]}]]]], options);
-});
